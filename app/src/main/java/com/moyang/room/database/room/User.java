301a730813f648d51d7fbbdee98a828794e5ff52
moyang <moyanghualing@163.com>
2022-08-07 23:15:40 +0800

    MNo: #20220707 DES: 学习数据传递 Type: Learn Project: No

--- app/src/main/java/com/moyang/room/database/room/User.java
+++ app/src/main/java/com/moyang/room/database/room/User.java
@@ -1,5 +1,8 @@
 package com.moyang.room.database.room;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
@@ -10,7 +13,7 @@ import androidx.room.PrimaryKey;
  * @date: 2022/8/7 19:56
  */
 @Entity
-public class User {
+public class User implements Parcelable {
     @PrimaryKey(autoGenerate = true)
     public int uid;
 
@@ -20,6 +23,39 @@ public class User {
     @ColumnInfo(name = "last_name")
     public String lastName;
 
+    public User() {
+    }
+
+    // 读,读和写的顺序必须一致
+    protected User(Parcel in) {
+        uid = in.readInt();
+        firstName = in.readString();
+        lastName = in.readString();
+    }
+
+    // 写
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(uid);
+        dest.writeString(firstName);
+        dest.writeString(lastName);
+    }
+
+    // 内部调用,一定要有
+    public static final Creator<User> CREATOR = new Creator<User>() {
+
+        // 通过Parcel创建对象
+        @Override
+        public User createFromParcel(Parcel in) {
+            return new User(in);
+        }
+
+        @Override
+        public User[] newArray(int size) {
+            return new User[size];
+        }
+    };
+
     @Override
     public String toString() {
         return "User{" +
@@ -28,4 +64,10 @@ public class User {
                 ", lastName='" + lastName + '\'' +
                 '}';
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
 }
